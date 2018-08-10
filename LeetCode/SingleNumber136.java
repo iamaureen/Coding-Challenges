2f0a521d45958beabc2ded077d97f4590064013d
iamaureen <ishrat.ahmed@asu.edu>
2018-08-10 12:24:56 -0700

    LeetCode/136. SingleNumber.java

---
+++ LeetCode/SingleNumber136.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+public class SingleNumber136 {
+	
+	public static int singleNumber(int[] nums) {
+		
+		int singleNum = 0;
+        
+        for (int i=0;i<nums.length; i++){
+            singleNum ^= nums[i];
+        }
+        
+        return singleNum;
+    
+    }
+	
+	public static void main(String[] args) {
+		int[] nums = {4,1,2,1,2};
+		System.out.println(singleNumber(nums));
+	}
+
+}
