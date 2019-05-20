1c2f9ac8d1809f0eb2d4ce5959fc9bdbda0d0cb2
iamaureen <ishrat.ahmed@asu.edu>
2019-05-20 19:08:33 -0400

    LeetCode/PalindromeNumber9

---
+++ LeetCode/PalindromeNumber9.java
@@ -0,0 +1,23 @@
+package LeetCode;
+
+public class PalindromeNumber9 {
+	
+	public static boolean isPalindrome(int x) {
+		
+		if(x<0) return false; 
+		int original_num = x;
+		int reverse_num = 0;
+		
+		while(x>0) {
+			reverse_num = reverse_num*10 + x%10;
+			x = x/10;
+		}
+		
+		return reverse_num==original_num;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(isPalindrome(98));
+	}
+
+}
