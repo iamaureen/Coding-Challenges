8a101669c12c7182ecbf9371ad1da9fe72d2460f
iamaureen <ishrat.ahmed@asu.edu>
2019-05-29 15:01:24 -0400

    LeetCode/flipAndInvertImage832

---
+++ LeetCode/flipAndInvertImage832.java
@@ -0,0 +1,29 @@
+package LeetCode;
+
+//problem:https://leetcode.com/problems/flipping-an-image
+//time: 20 minutes
+public class flipAndInvertImage832 {
+	
+	   public int[][] flipAndInvertImage(int[][] A) {
+	        
+		   int[][] flipped_image = new int[A.length][A[0].length];
+		   
+		   for(int i=0;i<A.length;i++) {
+			   int m = 0;
+			   for(int j=A[0].length-1;j>=0;j--) {
+				   flipped_image[i][m++] = A[i][j];
+			   }
+		   }
+		   
+		   for(int i=0;i<flipped_image.length;i++) {
+			   for(int j=0;j<flipped_image[0].length;j++) {
+				   if(A[i][j] == 0) A[i][j] = 1;
+		           else A[i][j] = 0;
+			   }
+				   
+		   }
+		   
+		   return flipped_image;
+	    }
+
+}
