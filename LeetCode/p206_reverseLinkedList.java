b579feaa48e04f4902e28e7602161ae034d84e52
iamaureen <ishrat.ahmed@asu.edu>
2020-09-15 21:13:26 -0400

    comment added for linked list reverse iterative solution

--- LeetCode/p206_reverseLinkedList.java
+++ LeetCode/p206_reverseLinkedList.java
@@ -17,21 +17,30 @@ public class p206_reverseLinkedList {
 	 public ListNode reverseList(ListNode head) {
 	        
 	        
-	        if(head == null) return null;
+		    //edge case
+	        if(head == null) return null; 
 	        
-	        ListNode reverse=null;
-	        ListNode current = head;
+	        //create a node to hold the recerse list
+	        ListNode reverse = null;
 	        
+	        //create a node to point to the beginning of the list 
+	        ListNode temp = head;
 	        
-	        while(current!=null){
-	            ListNode newNode = new ListNode(current.val);
-	            current = current.next;
+	        while(temp != null){
+	            //create a node with the current element of the list == i of the for loop
+	            ListNode current = new ListNode(temp.val);
+	            temp = temp.next; 
+	            
+	            //check if we have anything in the reverse list yet
 	            if(reverse == null){
-	                reverse = newNode;
+	                reverse = current; 
 	            }else{
-	                newNode.next = reverse;
-	                reverse = newNode;
+	                //we point the reverse list at the end of the current node
+	                current.next = reverse;
+	                //then we redirect reverse to the beginning of the current node
+	                reverse = current; 
 	            }
+	            
 	        }
 	        
 	        return reverse;
