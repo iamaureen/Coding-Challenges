0288e1dca46ddced8d1d37d5a997a0b86c3b4a2c
iamaureen <ishrat.ahmed@asu.edu>
2022-01-05 22:12:20 -0500

    find modes in BST using hashmap

--- LeetCode/p501_modeInBST.java
+++ LeetCode/p501_modeInBST.java
@@ -23,8 +23,10 @@ import java.util.*;
  */
 
 public class p501_modeInBST {
-//1 2 3 4 4 5 5 6 -- mode is 4,5 since 4, 5 both occurs two times. 
+	//1 2 3 4 4 5 5 6 -- mode is 4,5 since 4, 5 both occurs two times. 
     
+	//solution - using DFS/recursion
+	/*
     Integer prev = null;
     int count = 1;
     int max = 0; 
@@ -71,7 +73,46 @@ public class p501_modeInBST {
         prev = node.val;
        
         traverse(node.right, modes);
+    } */
+	
+	int maxCount = 1;
+    public int[] findMode(TreeNode root) {
+        
+        //using hashmap
+       Map<Integer, Integer> map = new HashMap<>();
+       
+       List<Integer> result = new ArrayList<Integer>();
+       traverse(root, map);
+        
+       for (int c: map.keySet()){
+           if(map.get(c) == maxCount) {
+               result.add(c);
+           }
+       }
+        
+        //convert list to array
+        //return result.toArray(new int[result.size()]); //this won't work because there's no                  conversion from Integer to in
+        return result.stream().mapToInt(Integer::intValue).toArray();
+        //Using the collections stream() function and then mapping to ints, you'll get an IntStream.           With the IntStream we can call toArray() which gives us int []
+        
     }
+   
+   public void traverse(TreeNode node, Map<Integer, Integer> map){
+       
+       if(node == null) return;
+       
+       if(map.containsKey(node.val)){
+           int count = map.get(node.val) + 1;
+           map.put(node.val, count);
+           maxCount = Math.max(maxCount, count);
+       }else{
+           map.put(node.val, 1);
+       }
+       
+       traverse(node.left, map);
+       traverse(node.right, map);
+   }
+   
 }
 
 
