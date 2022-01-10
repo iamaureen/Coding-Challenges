838f728464377b7ef3f31388a4430344b8a0c24e
iamaureen <ishrat.ahmed@asu.edu>
2022-01-09 22:27:09 -0500

    removing elements from linked list

--- LeetCode/p203_RemoveAllOccurances.java
+++ LeetCode/p203_RemoveAllOccurances.java
@@ -29,33 +29,57 @@ public class p203_RemoveAllOccurances {
 	
 	public static Node removeElements(Node head, int val) {
 		
-		//three cases
-        //1. value can be at the beginning of the linked list (head value)
-        //2. value at the middle of the linked list
-        //3. value at the end of the linked list 
-        
-        //case #1
-        while(head!=null && head.x == val){
-            head = head.next; 
-        }
-        
-        //to keep the original pointer to the head 
-        Node temp = head;
-        
-        //when we are at a node, we want to check if the next node val is equal to val, so we need to check whether both of them are not empty
-        while(temp!=null && temp.next!=null){
-            
-            //if the value is equal to the value to be removed
-            if(temp.next.x == val){
-                temp.next = temp.next.next;
-                
-            }else{
-                temp = temp.next;
-            }
-            
-        }
-        
-        return head; 
+		 if(head == null) return null;
+	        
+		 	Node prev = null;
+		 	Node current = head;
+	        
+	        while(current != null){
+	            
+	            if(current.x == val){
+	                
+	                //if the element is in the beginning, forward the head to the next element;
+	                if(current == head) head = current.next; 
+	                else prev.next = current.next; 
+	                
+	                current = current.next; 
+	                continue;
+	            }
+	            
+	            prev = current;
+	            current = current.next; 
+	            
+	        }
+	        
+	        return head;
+		
+//		//three cases
+//        //1. value can be at the beginning of the linked list (head value)
+//        //2. value at the middle of the linked list
+//        //3. value at the end of the linked list 
+//        
+//        //case #1
+//        while(head!=null && head.x == val){
+//            head = head.next; 
+//        }
+//        
+//        //to keep the original pointer to the head 
+//        Node temp = head;
+//        
+//        //when we are at a node, we want to check if the next node val is equal to val, so we need to check whether both of them are not empty
+//        while(temp!=null && temp.next!=null){
+//            
+//            //if the value is equal to the value to be removed
+//            if(temp.next.x == val){
+//                temp.next = temp.next.next;
+//                
+//            }else{
+//                temp = temp.next;
+//            }
+//            
+//        }
+//        
+//        return head; 
         
 		/* Node newHead = new Node(-1);
 		newHead.next = head;
