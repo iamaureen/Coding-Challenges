5068ac572a7645060cf5188d96c84d9d2043a885
iamaureen <ishrat.ahmed@asu.edu>
2022-01-11 23:49:26 -0500

    length of last word

--- LeetCode/p58_lengthOfLastWord.java
+++ LeetCode/p58_lengthOfLastWord.java
@@ -7,16 +7,22 @@ public class p58_lengthOfLastWord {
 	 public static int lengthOfLastWord(String s) {
 		
 		 
-		 String[] splitString = s.trim().split(" ");
-		 int length = splitString.length;
-		 String lastWord = splitString[length-1];
+//		 String[] splitString = s.trim().split(" ");
+//		 int length = splitString.length;
+//		 String lastWord = splitString[length-1];
+//		 
+//		 return lastWord.length();
 		 
-		 return lastWord.length();
+		 
+		 String[] splitArray = s.split("\\s+"); //split whenever at least one whitespace is encountered.
+		 int length = splitArray.length;  
+	     String lastword = splitArray[length-1];
+	     return lastword.length();
 	     
 	    }
 	 
 	 public static void main(String args[]) {
-		 System.out.println(lengthOfLastWord(" "));
+		 System.out.println(lengthOfLastWord("this is us"));
 	 }
 
 }
