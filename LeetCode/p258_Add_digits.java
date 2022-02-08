4e94458d009d78135d03c9ce862f4a5d5b2e9309
iamaureen <ishrat.ahmed@asu.edu>
2022-02-07 22:12:53 -0500

    add digits

--- LeetCode/p258_Add_digits.java
+++ LeetCode/p258_Add_digits.java
@@ -1,59 +1,33 @@
 package LeetCode;
 
 
-//problem definition: https://leetcode.com/problems/product-of-array-except-self/
+//problem definition: https://leetcode.com/problems/add-digits/
 
 public class p258_Add_digits {
-	 public int[] productExceptSelf(int[] nums) {
-	        
-//       //Brute Force Technique:
-//       //time: o(n^2)
-//       //space: o(1)
-//       //create an array for the result
-//       int[] result = new int[nums.length];
-      
-//       for(int i=0;i<nums.length;i++){
-//           int mult = 1; 
-//           for(int j=0;j<nums.length;j++){
-//               if(i==j) continue; 
-//               else{
-//                   mult *= nums[j];
-//               }
-//               result[i] = mult;
-//           }
-//       }
-      
-//       return result;
-      
-//   }
-      
-      //time: o(n) -- without using division
-      //https://leetcode.com/problems/product-of-array-except-self/solution/
-      int[] left_products = new int[nums.length];
-      int[] right_products = new int[nums.length];
-      
-      //output array
-      int[] output = new int[nums.length];
-      
-      left_products[0] = 1; 
-      right_products[nums.length-1] = 1;
-      
-      //forward loop and fill left product -- for each element multiply what is before that element
-      for(int i=1; i<nums.length;i++){
-          left_products[i] = nums[i-1]*left_products[i-1];
-      }
-      
-      //backward loop and fill right product -- for each element multiply what is after that element
-      for(int i=nums.length-2; i>=0;i--){
-          right_products[i] = nums[i+1]*right_products[i+1];
-      }
-      
-      for(int i=0;i<nums.length;i++) {
-          output[i] = left_products[i]*right_products[i];
-      }
-      
-      return output;
-  }
+public int addDigits(int num) {
+        
+        int sum = 0;
+        
+        
+        while(num > 0){
+            
+            sum += num % 10;
+            num = num / 10; 
+            
+            //need to add num == 0 otherwise wrong answer - check num=119
+            //at one iteration, the sum becomes greater than 9 however num remains 1 - w/o num == 0 it resets. 
+            //so need to add this.
+            if(num == 0 && sum>9){
+                num = sum;
+                sum = 0;
+            }
+            
+        
+        }
+        
+        return sum;
+        
+    }
 	
 
 }
