894c62e6fe472e0a9a6252a891222ebc9aeaa0da
iamaureen <ishrat.ahmed@asu.edu>
2022-02-19 00:53:48 -0500

    self-dividing number math problem

---
+++ LeetCode/p728_self_dividing_number.java
@@ -0,0 +1,43 @@
+package LeetCode;
+
+import java.util.*;
+
+//https:https://leetcode.com/problems/self-dividing-numbers/
+
+public class p728_self_dividing_number {
+	
+	public List<Integer> selfDividingNumbers(int left, int right) {
+	        
+	        List<Integer> result = new ArrayList<>();
+	        
+	        for(int i=left; i<=right; i++){
+	            
+	            boolean isSelfDividing = checkSelfDividing(i);
+	            
+	            if(isSelfDividing) result.add(i);
+	            
+	        }
+	        
+	        return result;
+	        
+	    }
+	    
+	    public boolean checkSelfDividing(int num){
+	        
+	        int original = num;
+	        
+	        while(num!=0){
+	            
+	            int divisor = num%10;
+	            num = num/10;
+	            
+	           
+	            if (divisor != 0 && original % divisor == 0) continue;
+	            else return false;
+	            
+	        }
+	        
+	        return true;
+	    }
+
+}
